9116a7d00e46fa1aeb8c1f5a498ce60010510de6
Ritika Nair <nair.r@husky.neu.edu>
2019-03-23 15:37:28 -0400

    implements bookmarking client side

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { UserServiceClient } from '../services/user.service.client';
 //import {} from '@types/googlemaps';
 import { Router } from '@angular/router';
 import { EventServiceClient } from '../services/event.service.client';
+import { BookmarkServiceClient } from '../services/bookmark.service.client';
 
 
 @Component({
@@ -18,12 +19,20 @@ export class HomeComponent implements OnInit {
     
     this.eventService.findAllEvents()
       .then(events => {
+
+        events.forEach(event => {
+          event.isBookmarked = true;
+        });
         console.log(events);
         this.events = events;
       });
   }
 
-  constructor(private userService: UserServiceClient, private router: Router, private eventService: EventServiceClient) {
+  constructor(private userService: UserServiceClient, 
+    private router: Router, 
+    private eventService: EventServiceClient,
+    private bookmarkService: BookmarkServiceClient
+    ) {
   }
 
 
@@ -32,6 +41,13 @@ export class HomeComponent implements OnInit {
   viewEvent = eventId => {
     //console.log(eventId);
     this.router.navigate(['/event/' + eventId]);
+  }
+
+  bookmark = event => {
+    event.isBookmarked = !event.isBookmarked;
+    console.log(event);
+    //TODO: invoke DATABASE
+   // this.router.navigate(['/event/' + eventId]);
   }
 
 
--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrgServiceClient } from '../services/org.service.client';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-organization',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orgService: OrgServiceClient,private activatedRoute: ActivatedRoute) { }
+
+  orgId : String;
+  org: {};
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.orgId = params['orgId'];
+    });
+   console.log(this.orgId);
+
+    this.orgService.findorgById(this.orgId)
+      .then(org => {
+        console.log(org);
+        this.org = org;
+        console.log(this.org);
+      });
   }
 
 }
