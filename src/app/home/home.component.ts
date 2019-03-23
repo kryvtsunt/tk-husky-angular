05c1868e2a6d942c7981ff44ff828b8557f8ea4a
Ritika Nair <nair.r@husky.neu.edu>
2019-03-23 14:36:17 -0400

    implements event list and event page retrieve

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {UserServiceClient} from '../services/user.service.client';
-import {} from '@types/googlemaps';
-import {Router} from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserServiceClient } from '../services/user.service.client';
+//import {} from '@types/googlemaps';
+import { Router } from '@angular/router';
+import { EventServiceClient } from '../services/event.service.client';
 
 
 @Component({
@@ -14,11 +15,26 @@ import {Router} from '@angular/router';
 
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
-
+    
+    this.eventService.findAllEvents()
+      .then(events => {
+        console.log(events);
+        this.events = events;
+      });
   }
 
-  constructor(private userService: UserServiceClient, private router: Router) {
+  constructor(private userService: UserServiceClient, private router: Router, private eventService: EventServiceClient) {
   }
+
+
+  events = [];
+
+  viewEvent = eventId => {
+    //console.log(eventId);
+    this.router.navigate(['/event/' + eventId]);
+  }
+
+
 
 
 }
