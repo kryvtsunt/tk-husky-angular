2e80a0fbfc2d2abd08a829d17047f105e564f56b
Ritika Nair <nair.r@husky.neu.edu>
2019-03-25 23:16:45 -0400

    bookmarked events show on profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookmarkServiceClient } from '../services/bookmark.service.client';
+import { UserServiceClient } from '../services/user.service.client';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  bookmarkedEvents = [];
+  constructor( private bookmarkService: BookmarkServiceClient, 
+    private userService : UserServiceClient) { }
 
   ngOnInit() {
+
+    this.userService.profile().then((user)=> {
+
+      this.bookmarkService.findAllBookmarkedevents(user._id).then((response)=>{
+        console.log(response);
+        this.bookmarkedEvents  =response;
+      })
+    })
+    
   }
 
 }
