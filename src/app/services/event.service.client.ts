ab725e1a62da6f8959793bf5f23837de2e4f982c
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-03-27 16:12:59 -0400

    homw styling, tags, and filtering

--- src/app/services/event.service.client.ts
+++ src/app/services/event.service.client.ts
@@ -1,8 +1,8 @@
 
 import {Injectable} from '@angular/core';
 
-const HOST = 'https://huskyneighborhood.herokuapp.com/';
-//const HOST = 'http://localhost:4000/';
+// const HOST = 'https://huskyneighborhood.herokuapp.com/';
+const HOST = 'http://localhost:4000/';
 
 @Injectable()
 export class EventServiceClient {
